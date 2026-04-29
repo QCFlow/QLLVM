3e2bd3a341a43d3b21927ecdc8add32e354fae62
yuqiong <19971935441@163.com>
2026-04-29 09:35:01 +0000

    删除文件 tpls/cmake

--- tpls/cmake/AddPathToRc.cmake
+++
@@ -1,3 +0,0 @@
-# Install-time script: Add ~/.qllvm/bin to the PATH
-# During runtime, the value of CMAKE_INSTALL_PREFIX represents the installation target path.
-include(${CMAKE_CURRENT_LIST_DIR}/PostInstallPath.cmake)
